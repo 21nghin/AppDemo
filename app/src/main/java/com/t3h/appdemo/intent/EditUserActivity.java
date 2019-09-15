0a3a45389e4473fe122fb86912e8e35f1497bc22
21nghin <nito21nghin@gmail.com>
2019-09-15 10:26:38 +0700

    push 15/9/2019 10h25AM

---
+++ app/src/main/java/com/t3h/appdemo/intent/EditUserActivity.java
@@ -0,0 +1,48 @@
+package com.t3h.appdemo.intent;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.t3h.appdemo.R;
+
+public class EditUserActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user);
+
+        toolbar = findViewById(R.id.id_edit_toolbar);
+        toolbar.setTitle(getString(R.string.tv_edit_user));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit_user,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.item_add:
+                Toast.makeText(this, "add check", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/t3h/appdemo/intent/UserActivity.java
+++ app/src/main/java/com/t3h/appdemo/intent/UserActivity.java
@@ -4,22 +4,35 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.t3h.appdemo.R;
 
-public class UserActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FloatingActionButton fabEdit;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
-        Toolbar toolbar = findViewById(R.id.user_toolbar);
+        toolbar = findViewById(R.id.user_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        initViews();
+
+    }
+
+    private void initViews() {
+        fabEdit = findViewById(R.id.fab_Edit);
+        fabEdit.setOnClickListener(this);
     }
 
     @Override
@@ -30,5 +43,10 @@ public class UserActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this,EditUserActivity.class));
     }
 }
